f060b13e098b288ef401eedd6dadb62fad74dfd3
pavel <pavelrazrushitel1ads@mail.ru>
2025-07-27 20:41:27 +0700

    - Added tailwind.config.js config - Main page UI layout completed

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -9,5 +9,52 @@ export const HomePage = () => {
   const initDataRaw = useSignal(_initDataRaw);
   const initDataState = useSignal(_initDataState);
 
-  return <Button className="text-3xl text-amber-700">A:das.fasf</Button>;
+  return (
+    <div className="w-full max-w-[425px] min-w-[320px] max-h-[642px] h-full">
+      <div className="bg-[#111217] py-[29px] px-2.5">
+        <div className="flex items-center flex-col w-full">
+          <section className="mb-[26px]">
+            <h1 className="uppercase text-[#535A64] text-lg font-medium">Ton balance</h1>
+            <p className="text-center text-[#FFFFFF] text-[28px] block leading-none">0.000000</p>
+          </section>
+
+          <div className="relative w-[263px] h-[263px] border-[6px] border-solid border-[#18A7FB] rounded-full">
+            <div
+              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+             flex items-center justify-center border-[8px] border-solid border-[#262c3a]
+             rounded-full w-[150px] h-[150px]">
+              <img
+                className="w-[100px] h-[100px] object-cover animate-spin"
+                src="../../../../public/images/Fan.png"
+                alt="Fan-image"
+              />
+            </div>
+          </div>
+
+
+          <section className="my-[26px]">
+            <h2 className="uppercase text-[#535A64] font-medium text-center">Passive income</h2>
+
+            <div className="flex gap-2.5 items-center">
+              <p className="text-[#18A7FB] text-[28px] block leading-none">0.000000</p>
+              <p className="text-[#18A7FB] text-[18px] block leading-none">TON/hr</p>
+            </div>
+          </section>
+        </div>
+
+        <div className="flex justify-between items-center mx-[30px]">
+          <h2 className="uppercase text-[#FFFFFF] font-medium">Level</h2>
+          <Button className="btn-main px-5 text-[#18A7FB] bg-[#1B1D29]">Upgrade</Button>
+        </div>
+
+        <div className="flex justify-center flex-wrap gap-[15px] mt-[58px]">
+          {/*TODO Повторы стилей, поскольку хз как настроить конфиг*/}
+          <Button className="btn-main px-3.5 text-[#18A7FB] bg-[#1B1D29]">Deposit</Button>
+          <Button className="btn-main px-3.5 text-[#18A7FB] bg-[#1B1D29]">Withdraw</Button>
+          <Button className="btn-main px-3.5 text-[#18A7FB] bg-[#1B1D29]">Invite</Button>
+          <Button className="btn-main px-3.5 text-[#18A7FB] bg-[#1B1D29]">Bonus</Button>
+        </div>
+      </div>
+    </div>
+  )
 };
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// const plugin = require('tailwindcss/plugin');
+//
+// module.exports = {
+//   content: ['./src/**/*.{js,ts,jsx,tsx}'],
+//   theme: {
+//     extend: {
+//       colors: {
+//         'primary-bg': '#1B1D29',
+//         'accent-blue': '#18A7FB',
+//       },
+//     },
+//   },
+//   plugins: [
+//     plugin(function () {
+//       console.log('✅ Tailwind конфиг загружен');
+//     }),
+//   ],
+// };
