298f268fdce5a6aa0f24005dee46acf6671d75b0
pavel <pavelrazrushitel1ads@mail.ru>
2025-07-29 02:35:46 +0700

    - Added BonusPage.tsx - Downloaded material library

---
+++ src/features/bonus/ui/BenefitCard.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@/shared/ui";
+import { ReactNode } from "react";
+
+interface BenefitCardProps {
+  description: string;
+  benefit: string;
+  icon: ReactNode;
+}
+
+export const BenefitCard = ({ description, benefit, icon }: BenefitCardProps) => {
+  return (
+    <div className="bg-[#161d27] p-3 h-[100px] rounded-[12px] mb-4">
+      <div className="flex items-center justify-between">
+        <div className="flex gap-5 items-center">
+          <div className="bg-[#2180d4] p-1.5 w-[60px] h-[60px] rounded-[12px]">
+            { icon }
+          </div>
+
+          <p className="max-w-[100px]">{ description }</p>
+        </div>
+
+        <Button className="bg-[#1f2533] uppercase font-semibold text-[18px]">Get</Button>
+      </div>
+
+      <p className="flex justify-end text-[#18A7FB] font-semibold">{ benefit }</p>
+    </div>
+  );
+};
--- src/pages/bonus/ui/BonusPage.tsx
+++ src/pages/bonus/ui/BonusPage.tsx
@@ -1,9 +1,24 @@
 import { Page } from "@/shared/ui";
+import { Bot } from "lucide-react";
+import { BenefitCard } from "@/features";
+import TelegramIcon from '@mui/icons-material/Telegram';
 
 export const BonusPage = () => {
   return (
-    <Page>
-      Bonus page
+    <Page back={ false } className="flex flex-col items-center gap-y-6">
+      <h1 className="text-5xl text-[#5c8afa] uppercase font-semibold text-center mb-[20px]">Bonus</h1>
+
+      <section className="w-full">
+        <h2 className="text-[#535A64] uppercase font-semibold mb-2">Opportunities</h2>
+
+        <BenefitCard description="Subscribe to the bot" benefit="+ 0.15 TON"
+                     icon={ <Bot className="w-full h-full stroke-[#151c26]"/> }
+        />
+
+        <BenefitCard description="Subscribe to the channel" benefit="+ 0.20 TON"
+                     icon={ <TelegramIcon className="!text-[48px] text-[#151c26]"/> }
+        />
+      </section>
     </Page>
   );
 };
